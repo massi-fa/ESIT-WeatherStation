070266feb3330c9cc4c602d843f0b1f674901ea3
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2022-08-09 16:26:45 +0200

    DHT sensor and refresh time

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 4   // Pin a cui è connesso il sensore
+#define DHTTYPE DHT22   // Tipo di sensore che stiamo utilizzando (DHT22)
+#define REFRESH 20000 // Intervallo di tempo tra un refresh e l'altro
+
+DHT dht(DHTPIN, DHTTYPE); // Parametri: pin a cui è connesso il sensore, tipo di dht 11/22
+
+//Variabili
+unsigned long myTime; //Variabile per salvare il tempo passato dall'inizio dell'esecuzione del programma
+float hum;  //Variabile in cui verrà inserita la % di umidità
+float temp; //Variabile in cui verrà inserita la temperatura
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  myTime = millis();
+
+  if (myTime%REFRESH == 0) { //Se il tempo passato dall'inizio dell'esecuzione del programma è multiplo di REFRESH
+    hum = dht.readHumidity(); //Leggo l'umidità
+    temp = dht.readTemperature(); //Leggo la temperatura
+    Serial.print("Umidità: ");
+    Serial.print(hum);
+    Serial.print(" %\t");
+    Serial.print("Temperatura: ");
+    Serial.print(temp);
+    Serial.println(" °C");
+  }
+
 }
